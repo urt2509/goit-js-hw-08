e3427737c33637a7a12ccd23ec1db9bb216c2f89
Alla <urt2509@gmail.com>
2023-04-13 23:25:12 +0200

    updated 01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,34 @@
-// Add imports above this line
+// Add imports from SimpleLightbox
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
 
-console.log("object");
+const galleryList = document.querySelector('.gallery');
+
+/**
+ * Function creates HTML for gallery of images
+ */
+const markup = galleryItems.map(
+  ({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+);
+
+/**
+ * Function insert created markup into HTML
+ */
+galleryList.insertAdjacentHTML('beforeend', markup.join(''));
+
+/**
+ * Create an instance for image lightbox
+ */
+const galleryListLightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+
+  captionDelay: 250,
+});
