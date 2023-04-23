6f0b803fa4003ca748a079b8617e960c90b51d73
Alla <urt2509@gmail.com>
2023-04-23 13:57:04 +0200

    task2 udated

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Vimeo.Player(iframe);
+
+function setLocalStorage(data) {
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data.seconds));
+}
+player.on('timeupdate', throttle(setLocalStorage, 1000));
+
+try {
+  player
+    .setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY))
+    .then(function (seconds) {
+      JSON.parse(seconds);
+    });
+} catch (error) {
+  console.log(error.message);
+}
